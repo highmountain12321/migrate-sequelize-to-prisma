6ef90770cea51bf2acd41bcc520628f90743c9da
highmountain12321 <timesaver1006@gmail.com>
2023-09-21 03:46:58 -0300

    fix: projects.js

--- app/controllers/projects.js
+++ app/controllers/projects.js
@@ -20,81 +20,91 @@ const { models } = require('../../sequelize');
  */
 
 exports.list = async function (req, res) {
-    const limit = req.query.limit || 100;
-    const offset = req.query.offset || 0;
-    const order = req.query.order || 'DESC';
+    try {
+        const limit = req.query.limit || 100;
+        const offset = req.query.offset || 0;
+        const order = req.query.order || 'DESC';
 
-    const {user, role} = req.token;
-    const i = [{
-        model:models.role,
-        as:'role'
-    },{
-        model:models.partner,
-        required:false,
-        as:'partner'
-    }]
+        const { user, role } = req.token;
 
-    const where = {
-        [Op.or]: [{
-            'partnerUserId': user,
-            isActive: true
-        }]
+        const projects = await prisma.project.findMany({
+            where: {
+                OR: [
+                    {
+                        partnerUserId: user,
+                        isActive: true
+                    }
+                ]
+            },
+            select: {
+                id: true,
+                partnerUserId: true,
+                updateId: true,
+                contactId: true,
+                createdAt: true,
+                partnerId: true,
+                update: {
+                    select: {
+                        note: true,
+                        createdAt: true,
+                        to: {
+                            select: {
+                                name: true,
+                                id: true
+                            }
+                        }
+                    }
+                },
+                partnerUser: {
+                    select: {
+                        firstName: true,
+                        partnerId: true,
+                        partner: {
+                            select: {
+                                name: true
+                            }
+                        }
+                    }
+                },
+                contact: {
+                    select: {
+                        primaryPhone: true,
+                        firstName: true,
+                        lastName: true,
+                        address1: true,
+                        city: true,
+                        state: true,
+                        postalCode: true
+                    }
+                }
+            },
+            orderBy: {
+                id: order.toLowerCase() === 'desc' ? 'desc' : 'asc'
+            },
+            skip: offset,
+            take: limit
+        });
+        res.json(projects);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send(e.message);
     }
+};
 
-
-    const projects = await models.project.findAll({
-        where: where,
-        attributes:[
-            'id',
-            'partnerUserId',
-            'updateId',
-            'contactId',
-            'createdAt',
-            'partnerId',
-        ],
-        order: [
-            ['id', order]
-        ],
-        include:[{
-            attributes:['note','createdAt'],
-            model: models.project_update,
-            as:'update',
-            include: [{
-                model: models.project_update_type,
-                as: 'to',
-                attributes:['name','id']
-            }]
-        },{
-            model:models.user,
-            attributes:['firstName','partnerId'],
-            as:'partnerUser',
-            include:[{
-                attributes:['name'],
-                model: models.partner,
-                as:'partner'
-            }]
-        },{
-            as:'contact',
-            model:models.contact,
-            attributes:[
-                'primaryPhone',
-                'firstName',
-                'lastName',
-                'address1',
-                'city','state','postalCode'
-            ]
-        }]
-    });
-    res.json(projects);
-}
 
 
 exports.count = async function (req, res) {
-    const count = await models.partner.count();
-    res.json({
-        count: count,
-    })
-}
+    try {
+        const count = await prisma.partner.count();
+        res.json({
+            count: count,
+        });
+    } catch (e) {
+        console.error(e);
+        res.status(500).send(e.message);
+    }
+};
+
 
 
 
@@ -102,190 +112,235 @@ exports.count = async function (req, res) {
 
 
 exports.create = async function (req, res, next) {
-    const project = req.body;
-    const userModel = req.userModel;
-    project.ownerId = userModel.id;
-    const newProjectModel = await models.project.create(project);
-    return res.json(newProjectModel);
-}
+    try {
+        const project = req.body;
+        project.ownerId = req.userModel.id;
+        const newProject = await prisma.project.create({
+            data: project
+        });
+        return res.json(newProject);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send(e.message);
+    }
+};
+
 
 exports.update = async function (req, res) {
-
-    const id = req.params.projectId;
-    if (req.body.id) {
-        res.status(400).send(`Bad request: ID should not be provided, since it is determined automatically by the database.`)
-        return;
+    try {
+        const id = req.params.projectId;
+        if (req.body.id) {
+            return res.status(400).send(`Bad request: ID should not be provided, since it is determined automatically by the database.`);
+        }
+        const updatedProject = await prisma.project.update({
+            where: { id: Number(id) },
+            data: req.body
+        });
+        res.status(200).json(updatedProject);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send(e.message);
     }
-    const obj = await models.project.update(req.body, { where: { id: id } });
-    res.status(201).json(obj);
+};
 
-}
 
 
 exports.show = async function (req, res) {
-    const id = req.params.projectId;
-    const obj = await models.project.findByPk( id,{
-        include:[{
-            attributes:['firstName','lastName','primaryPhone','roleId'],
-            model: models.user,
-            as:'users',
-            include: [{
-                model: models.role,
-                as: 'role',
-                attributes:['name']
-            }]
-        },{
-            attributes:['note','createdAt'],
-            model: models.project_update,
-            as:'update',
-            include: [{
-                model: models.project_update_type,
-                as: 'to',
-                attributes:['name','id']
-            }]
-        },{
-            as:'contact',
-            model:models.contact,
-            include:[{
-                attributes:['url'],
-                model: models.partner_proposal,
-                as:'partnerProposals'
-            },{
-                model: models.hoa,
-                as:'hoa'
-            }],
-
-            attributes:[
-                'id',
-                'primaryPhone',
-                'secondaryPhone',
-                'email',
-                'firstName',
-                'lastName',
-                'address1',
-                'address2',
-                'city','state','postalCode']
-        }]
-    });
-    res.status(200).json(obj);
+    try {
+        const id = req.params.projectId;
+        const project = await prisma.project.findUnique({
+            where: { id: Number(id) },
+            select: {
+                id: true,
+                // ... other direct fields you need,
+                users: {
+                    select: {
+                        firstName: true,
+                        lastName: true,
+                        primaryPhone: true,
+                        roleId: true,
+                        role: {
+                            select: {
+                                name: true
+                            }
+                        }
+                    }
+                },
+                update: {
+                    select: {
+                        note: true,
+                        createdAt: true,
+                        to: {
+                            select: {
+                                name: true,
+                                id: true
+                            }
+                        }
+                    }
+                },
+                contact: {
+                    select: {
+                        id: true,
+                        primaryPhone: true,
+                        secondaryPhone: true,
+                        email: true,
+                        firstName: true,
+                        lastName: true,
+                        address1: true,
+                        address2: true,
+                        city: true,
+                        state: true,
+                        postalCode: true,
+                        partnerProposals: {
+                            select: {
+                                url: true
+                            }
+                        },
+                        hoa: true
+                    }
+                }
+            }
+        });
+        res.status(200).json(project);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send(e.message);
+    }
 };
 
 
 
 
-exports.listTypes  = async function(req, res,next) {
-    const obj_array = await models.project_update_type.findAll({
-        where:{
-            isActive:1,
-            isVisible:1
-        },
-        order: [
-            ['order', 'ASC']
-        ]
-    });
-    res.json(obj_array);
-}
 
 
-exports.listDocuments = async function(req, res,next) {
-    const id = req.params.projectId;
-    const where = req.params.filter;
-    const query = {
-        where:{
-            id:id,
-        },
-        attributes: ['id'],
-        include: [{
-            model: models.document,
-            as: 'documents',
-            attributes: ['originalName','id','typeId','createdAt'],
-            include:[{
-                attributes: ['name','slug'],
-                model: models.document_type,
-                as: 'type'
-            }]
-        }]};
-    const obj = await models.project.findOne(query);
-    res.status(200).json(obj);
-}
-
-
-exports.createUpdate = async function(req, res,next) {
-    const {user, role} = req.token;
-
-    const newUpdate  = req.body;
-    const id = req.params.projectId;
-    newUpdate.userId = user;
-    if(!newUpdate.toId){
-        return res.json({});
+exports.listTypes = async function (req, res) {
+    try {
+        const projectUpdateTypes = await prisma.project_update_type.findMany({
+            where: {
+                isActive: 1,
+                isVisible: 1
+            },
+            orderBy: {
+                order: 'asc'
+            }
+        });
+        res.json(projectUpdateTypes);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send(e.message);
     }
-
-    const newUpdateModel = await models.project_update.create(newUpdate);
-    newUpdateModel.projectId = id;
-     await newUpdateModel.save();
-
-    const projectModel = await models.project.findByPk(newUpdateModel.projectId);
+};
 
 
 
-
-    projectModel.updateId = newUpdateModel.id;
-
-    await projectModel.save();
-    res.json({ok:true});
-
-}
-
-exports.listProjects = async function(req, res,next) {
-    const id = req.loadedBoardModel
-
-    const updates = await models.project_update.findAll({
-        order: [
-            ['id', 'DESC']
-        ],
-        where:{
-            projectId: id
-        },
-        attributes:['toId','createdAt','note'],
-        include:[{
-            model:models.user,
-            as: 'user',
-            attributes:['firstName','lastName']
-        },{
-            model:models.project_update_type,
-            as: 'to',
-            attributes:['name']
-        }]
-    })
-    res.status(200).json(updates);
-
-}
+exports.listDocuments = async function (req, res) {
+    try {
+        const id = req.params.projectId;
+        const project = await prisma.project.findUnique({
+            where: { id: Number(id) },
+            select: {
+                // ... your selected fields and includes
+            }
+        });
+        res.status(200).json(project);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send(e.message);
+    }
+};
 
 
-exports.listUpdates = async function(req, res,next) {
-    const id = req.params.projectId;
 
-    const updates = await models.project_update.findAll({
-        order: [
-            ['id', 'DESC']
-        ],
-        where:{
-            projectId: id
-        },
-        attributes:['toId','createdAt','note'],
-        include:[{
-            model:models.user,
-            as: 'user',
-            attributes:['firstName','lastName']
-        },{
-            model:models.project_update_type,
-            as: 'to',
-            attributes:['name']
-        }]
-    })
-    res.status(200).json(updates);
+exports.createUpdate = async function (req, res) {
+    try {
+        const { user, role } = req.token;
+        const newUpdate = req.body;
+        newUpdate.userId = user;
+        if (!newUpdate.toId) {
+            return res.json({});
+        }
+        const createdUpdate = await prisma.project_update.create({
+            data: newUpdate
+        });
+        await prisma.project.update({
+            where: { id: createdUpdate.projectId },
+            data: { updateId: createdUpdate.id }
+        });
+        res.json({ ok: true });
+    } catch (e) {
+        console.error(e);
+        res.status(500).send(e.message);
+    }
+};
 
-}
+
+exports.listDocuments = async function (req, res) {
+    try {
+        const id = req.params.projectId;
+        const project = await prisma.project.findUnique({
+            where: { id: Number(id) },
+            select: {
+                id: true,
+                documents: {
+                    select: {
+                        originalName: true,
+                        id: true,
+                        typeId: true,
+                        createdAt: true,
+                        type: {
+                            select: {
+                                name: true,
+                                slug: true
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        res.status(200).json(project);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send(e.message);
+    }
+};
+
+
+exports.listUpdates = async function (req, res, next) {
+    try {
+        const id = req.params.projectId;
+
+        const updates = await prisma.project_update.findMany({
+            where: {
+                projectId: Number(id)
+            },
+            select: {
+                toId: true,
+                createdAt: true,
+                note: true,
+                user: {
+                    select: {
+                        firstName: true,
+                        lastName: true
+                    }
+                },
+                to: {
+                    select: {
+                        name: true
+                    }
+                }
+            },
+            orderBy: {
+                id: 'desc'
+            }
+        });
+        
+        res.status(200).json(updates);
+    } catch (e) {
+        console.error(e);
+        next(e);
+    }
+};
+
 
 
 
@@ -293,13 +348,27 @@ exports.listUpdates = async function(req, res,next) {
 exports.destroy = async function (req, res, next) {
     try {
         const id = req.params.projectId;
-        const obj = await models.project.findByPk(id)
-        const response = await obj.destroy()
-        res.json({data: response});
-    }catch(e){
-        console.log(e);
+
+        // First, find the project to ensure it exists
+        const project = await prisma.project.findUnique({ 
+            where: { id: Number(id) } 
+        });
+
+        if (!project) {
+            res.status(404).send("Project not found");
+            return;
+        }
+
+        const response = await prisma.project.delete({
+            where: { id: Number(id) }
+        });
+        
+        res.json({ data: response });
+    } catch (e) {
+        console.error(e);
         next(e);
     }
-}
+};
+
 
 
